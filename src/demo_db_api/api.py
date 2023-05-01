87a281591b6654b775ec2d0e8d6201ea2268626b
Reginald Maltais <reginald.maltais@statcan.gc.ca>
2023-05-01 15:10:48 -0400

    Add app metadata

--- src/demo_db_api/api.py
+++ src/demo_db_api/api.py
@@ -1,6 +1,16 @@
+from importlib.metadata import version
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Demo cookiecutter API",
+    description="Demo of an API. Created from the StatCan datascience-cookiecutter.",
+    version=version("demo_db_api"),
+    license_info={
+        "name": "MIT",
+        "url": "https://github.com/goatsweater/demo-cookiecutter-api/blob/main/LICENSE"
+    }
+)
 
 
 @app.get("/")
